482b98161dcf4b4be6ff80f8b264615f025df020
aiekick <aiekick@funparadigm.com>
2024-02-25 10:06:05 +0100

    first commit

---
+++ VdbWriter.cpp
@@ -0,0 +1,384 @@
+#include "VdbWriter.h"
+#include <vector>
+#include <iomanip>
+#include <iostream>
+
+namespace vdb {
+
+void write_ptr(FILE* fp, const void* data, size_t elementSize, size_t count = 1) { fwrite(data, elementSize, count, fp); }
+void write_ptr(FILE* fp, void* data, size_t elementSize, size_t count = 1) { fwrite(data, elementSize, count, fp); }
+template<typename T>
+void write_data(FILE* fp, T data) {
+    write_ptr(fp, &data, sizeof(T));
+}
+template <typename T>
+void write_data_arr(FILE* fp, T* data, size_t count) {
+    write_ptr(fp, data, sizeof(T), count);
+}
+template <typename T>
+void write_data_arr(FILE * fp, const T* data, size_t count) {
+    write_ptr(fp, data, sizeof(T), count);
+}
+
+void write_string(FILE* fp, std::string str) { write_ptr(fp, str.data(), sizeof(uint8_t), str.size()); }
+void write_vec3i(FILE* fp, const std::array<int32_t, 3>& data) { write_data_arr<int32_t>(fp, data.data(), 3); }
+
+void write_name(FILE* fp, const std::string& name) {
+    write_data<uint32_t>(fp, (uint32_t)name.size());
+    write_string(fp, name);
+}
+
+void write_meta_string(FILE* fp, const std::string& name, const std::string& str) {
+    write_name(fp, name);
+    write_name(fp, "string");
+    write_name(fp, str);
+}
+
+void write_meta_bool(FILE* fp, const std::string& name, bool flag) {
+    write_name(fp, name);
+    write_name(fp, "bool");
+    write_data<uint32_t>(fp, 1);  // one byte is used to store the bool
+    write_data<uint8_t>(fp, flag ? 1 : 0);
+}
+
+void write_meta_vec3i(FILE* fp, const std::string& name, const std::array<int32_t, 3>& data) {
+    write_name(fp, name);
+    write_name(fp, "vec3i");
+    write_data<uint32_t>(fp, 12U);  // 12 bytes (4 bytes * 3) are used to store the vec3i
+    write_vec3i(fp, data);
+}
+
+uint32_t getBitIndex4(uint32_t x, uint32_t y, uint32_t z) {
+    x &= (uint32_t)(4096 - 1);
+    y &= (uint32_t)(4096 - 1);
+    z &= (uint32_t)(4096 - 1);
+    uint32_t idx_3d[3] = {x >> 7, y >> 7, z >> 7};
+    uint64_t idx       = idx_3d[2] | (idx_3d[1] << 5) | (idx_3d[0] << 10);
+    return idx;
+}
+
+uint32_t getBitIndex3(uint32_t x, uint32_t y, uint32_t z) {
+    x &= (uint32_t)(128 - 1);
+    y &= (uint32_t)(128 - 1);
+    z &= (uint32_t)(128 - 1);
+    uint32_t idx_3d[3] = {x >> 3, y >> 3, z >> 3};
+    uint64_t idx       = idx_3d[2] | (idx_3d[1] << 4) | (idx_3d[0] << 8);
+    return idx;
+}
+
+uint32_t getBitIndex0(uint32_t x, uint32_t y, uint32_t z) {
+    x &= (uint32_t)(8 - 1);
+    y &= (uint32_t)(8 - 1);
+    z &= (uint32_t)(8 - 1);
+    uint32_t idx_3d[3] = {x >> 0, y >> 0, z >> 0};
+    uint64_t idx       = idx_3d[2] | (idx_3d[1] << 3) | (idx_3d[0] << 6);
+    return idx;
+}
+
+void VdbWriter::saveToFile(const std::string& vFilePathName) {
+    if (!vFilePathName.empty()) {
+        auto dot_p = vFilePathName.find_last_of('.');
+        if (dot_p != std::string::npos) {
+            auto   base_file_path_name = vFilePathName.substr(0, dot_p) + "_";
+            size_t idx                 = 1;
+            for (auto& vdb : m_Vdbs) {
+                std::stringstream str;
+                str << base_file_path_name << std::setfill('0') << std::setw(4) << idx++ << ".vdb";
+                if (m_OpenFileForWriting(str.str())) {
+                    auto   center = maxVolume.GetCenter();
+                    Mat4x4 mat_identity;
+                    mat_identity[0] = {1, 0, 0, -center.x};
+                    mat_identity[1] = {0, 1, 0, -center.y};
+                    mat_identity[2] = {0, 0, 1, -center.z};
+                    mat_identity[3] = {0, 0, 0, 1};
+                    m_WriteVdb(m_File, &vdb.second, mat_identity);
+                    m_CloseFile();
+                }
+            }
+        }
+    }
+}
+
+void VdbWriter::printStats() const {
+    std::cout << "---- Stats ------------------------------" << std::endl;
+    std::cout << "Volume : " << maxVolume.Size().x << " x " << maxVolume.Size().y << " x " << maxVolume.Size().z << std::endl;
+    /*std::cout << "count cubes : " << cubes.size() << std::endl;
+    std::map<KeyFrame, size_t> frame_counts;
+    for (const auto& cube : cubes) {
+        for (auto& key_xyzi : cube.xyzis) {
+            frame_counts[key_xyzi.first] += key_xyzi.second.numVoxels;
+        }
+    }*/
+    /*size_t voxels_total = 0U;
+    if (frame_counts.size() > 1U) {
+        std::cout << "count key frames : " << frame_counts.size() << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+        for (const auto& frame_count : frame_counts) {
+            std::cout << " o--\\-> key frame : " << frame_count.first << std::endl;
+            std::cout << "     \\-> voxels count : " << frame_count.second << std::endl;
+            if (m_FrameTimes.find(frame_count.first) != m_FrameTimes.end()) {
+                std::cout << "      \\-> elapsed time : " << m_FrameTimes.at(frame_count.first) << " secs" << std::endl;
+            }
+            voxels_total += frame_count.second;
+        }
+        std::cout << "-----------------------------------------" << std::endl;
+    } else if (!frame_counts.empty()) {
+        voxels_total = frame_counts.begin()->second;
+    }
+    std::cout << "voxels total : " << voxels_total << std::endl;*/
+    std::cout << "total elapsed time : " << m_TotalTime << " secs" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+}
+
+void VdbWriter::addVoxelDensity(const uint32_t& x, const uint32_t& y, const uint32_t& z, float value) {
+    maxVolume.Combine(dvec3((float)x, (float)y, (float)z));
+    auto  bit_index_4 = getBitIndex4(x, y, z);
+    auto  bit_index_3 = getBitIndex3(x, y, z);
+    auto  bit_index_0 = getBitIndex0(x, y, z);
+    auto& nodes5Ref   = m_Vdbs[m_KeyFrame].nodes;
+    auto& nodes4Ref   = nodes5Ref.nodes[bit_index_4];
+    auto& nodes3Ref   = nodes4Ref.nodes[bit_index_3];
+    nodes5Ref.mask[bit_index_4 >> 6] |= static_cast<uint64_t>(1) << (bit_index_4 & (64 - 1));
+    nodes4Ref.mask[bit_index_3 >> 6] |= static_cast<uint64_t>(1) << (bit_index_3 & (64 - 1));
+    nodes3Ref.mask[bit_index_0 >> 6] |= static_cast<uint64_t>(1) << (bit_index_0 & (64 - 1));
+    nodes3Ref.data[bit_index_0] = value;
+}
+
+void VdbWriter::addVoxelNormal(const uint32_t& x, const uint32_t& y, const uint32_t& z, fVec3 normal) { 
+    /*maxVolume.Combine(dvec3((float)x, (float)y, (float)z));
+    auto  bit_index_4 = getBitIndex4(x, y, z);
+    auto  bit_index_3 = getBitIndex3(x, y, z);
+    auto  bit_index_0 = getBitIndex0(x, y, z);
+    auto& nodes5Ref   = m_Vdb.nodes;
+    auto& nodes4Ref   = nodes5Ref.nodes[bit_index_4];
+    auto& nodes3Ref   = nodes4Ref.nodes[bit_index_3];
+    nodes5Ref.mask[bit_index_4 >> 6] |= static_cast<uint64_t>(1) << (bit_index_4 & (64 - 1));
+    nodes4Ref.mask[bit_index_3 >> 6] |= static_cast<uint64_t>(1) << (bit_index_3 & (64 - 1));
+    nodes3Ref.mask[bit_index_0 >> 6] |= static_cast<uint64_t>(1) << (bit_index_0 & (64 - 1));
+    nodes3Ref.data[bit_index_0] = value;*/ }
+
+void VdbWriter::addVoxelColor(const uint32_t& x, const uint32_t& y, const uint32_t& z, fVec4 color) { 
+    /*maxVolume.Combine(dvec3((float)x, (float)y, (float)z));
+    auto  bit_index_4 = getBitIndex4(x, y, z);
+    auto  bit_index_3 = getBitIndex3(x, y, z);
+    auto  bit_index_0 = getBitIndex0(x, y, z);
+    auto& nodes5Ref   = m_Vdb.nodes;
+    auto& nodes4Ref   = nodes5Ref.nodes[bit_index_4];
+    auto& nodes3Ref   = nodes4Ref.nodes[bit_index_3];
+    nodes5Ref.mask[bit_index_4 >> 6] |= static_cast<uint64_t>(1) << (bit_index_4 & (64 - 1));
+    nodes4Ref.mask[bit_index_3 >> 6] |= static_cast<uint64_t>(1) << (bit_index_3 & (64 - 1));
+    nodes3Ref.mask[bit_index_0 >> 6] |= static_cast<uint64_t>(1) << (bit_index_0 & (64 - 1));
+    nodes3Ref.data[bit_index_0] = value;*/
+
+}
+
+void VdbWriter::startTimeLogging() {
+    m_TimeLoggingEnabled = true;
+    m_StartTime          = std::chrono::steady_clock::now();
+    m_LastKeyFrameTime   = m_StartTime;
+};
+
+void VdbWriter::stopTimeLogging() {
+    if (m_TimeLoggingEnabled) {
+        const auto now           = std::chrono::steady_clock::now();
+        m_FrameTimes[m_KeyFrame] = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_LastKeyFrameTime).count() * 1e-3;
+        if (m_KeyFrameTimeLoggingFunctor) {
+            m_KeyFrameTimeLoggingFunctor(m_KeyFrame, m_FrameTimes.at(m_KeyFrame));
+        }
+        m_TotalTime          = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_StartTime).count() * 1e-3;
+        m_TimeLoggingEnabled = false;
+    }
+}
+
+void VdbWriter::setKeyFrameTimeLoggingFunctor(const KeyFrameTimeLoggingFunctor& vKeyFrameTimeLoggingFunctor) {
+    m_KeyFrameTimeLoggingFunctor = vKeyFrameTimeLoggingFunctor;
+}
+
+void VdbWriter::setKeyFrame(uint32_t vKeyFrame) {
+    if (m_KeyFrame != vKeyFrame) {
+        if (m_TimeLoggingEnabled) {
+            const auto now           = std::chrono::steady_clock::now();
+            const auto elapsed       = now - m_LastKeyFrameTime;
+            m_FrameTimes[m_KeyFrame] = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() * 1e-3;
+            if (m_KeyFrameTimeLoggingFunctor) {
+                m_KeyFrameTimeLoggingFunctor(m_KeyFrame, m_FrameTimes.at(m_KeyFrame));
+            }
+            m_LastKeyFrameTime = now;
+        }
+        m_KeyFrame = vKeyFrame;
+    }
+}
+
+// Routines for writing the actual format
+void VdbWriter::m_WriteNode5Header(FILE* fp, const Node5& node) {
+    // Origin of the 5-node
+    write_vec3i(fp, {0, 0, 0});    
+    // Child masks
+    write_data_arr<uint64_t>(fp, node.mask, 512);
+    // Value masks are zero for now
+    static std::vector<uint64_t> mask_arr_uint64_empty(512);  // we dont use a std::array for not increase the bin size
+    write_data_arr<uint64_t>(fp, mask_arr_uint64_empty.data(), mask_arr_uint64_empty.size());
+    // Write uncompressed node values, 6 means no compression
+    write_data<uint8_t>(fp, 6);
+    static std::vector<float> mask_arr_float_empty(32768);  // we dont use a std::array for not increase the bin size
+    write_data_arr<float>(fp, mask_arr_float_empty.data(), mask_arr_float_empty.size());
+}
+
+void VdbWriter::m_WriteNode4Header(FILE* fp, const Node4& node) {
+    // Child masks
+    write_data_arr<uint64_t>(fp, node.mask, 64);
+    // Value masks are zero for now
+    static std::vector<uint64_t> mask_arr_uint64_empty(64);  // we dont use a std::array for not increase the bin size
+    write_data_arr<uint64_t>(fp, mask_arr_uint64_empty.data(), mask_arr_uint64_empty.size());
+    // Write uncompressed node values, 6 means no compression
+    write_data<uint8_t>(fp, 6);
+    static std::vector<float> mask_arr_float_empty(4096);  // we dont use a std::array for not increase the bin size
+    write_data_arr<float>(fp, mask_arr_float_empty.data(), mask_arr_float_empty.size());
+}
+
+template <typename T>
+int count_trailing_zeros(T value) {
+    int count = 0;
+    while ((value & 1) == 0 && value != 0) {
+        ++count;
+        value >>= 1;
+    }
+    return count;
+}
+
+void VdbWriter::m_WriteTree(FILE* fp, VDB* vdb) {
+    // We need to write a 1, apparently
+    write_data<uint32_t>(fp, 1);
+    // Root node background value
+    write_data<float>(fp, 0);
+    // Number of tiles
+    write_data<uint32_t>(fp, 0);
+    // Number of 5-nodes
+    write_data<uint32_t>(fp, 1);
+    const auto& nodes5Ref = vdb->nodes;
+    m_WriteNode5Header(fp, nodes5Ref);
+    // Iterate 4-nodes
+    size_t word5_idx = 0;
+    for (auto word5 : nodes5Ref.mask) {
+        const auto& base_bit_4_idx = ((uint32_t)word5_idx++) * 64;
+        for (; word5 != 0; word5 &= word5 - 1) {
+            const auto& bit_4_index = base_bit_4_idx + (uint32_t)count_trailing_zeros(word5);
+            const auto& nodes4Ref   = nodes5Ref.nodes.at(bit_4_index);
+            m_WriteNode4Header(fp, nodes4Ref);
+            // Iterate 3-nodes
+            size_t word4_idx = 0;
+            for (auto word4 : nodes4Ref.mask) {
+                const auto& base_bit_3_idx = ((uint32_t)word4_idx++) * 64;
+                for (; word4 != 0; word4 &= word4 - 1) {
+                    const auto& bit_3_index = base_bit_3_idx + (uint32_t)count_trailing_zeros(word4);
+                    const auto& nodes3Ref   = nodes4Ref.nodes.at(bit_3_index);
+                    write_data_arr<uint64_t>(fp, nodes3Ref.mask, 8);
+                }
+            }
+        }
+    }
+    // Iterate 4-nodes
+    word5_idx = 0;
+    for (auto word5 : nodes5Ref.mask) {
+        const auto& base_bit_4_idx = ((uint32_t)word5_idx++) * 64;
+        for (; word5 != 0; word5 &= word5 - 1) {
+            const auto& bit_4_index = base_bit_4_idx + (uint32_t)count_trailing_zeros(word5);
+            const auto& nodes4Ref   = nodes5Ref.nodes.at(bit_4_index);
+            // Iterate 3-nodes
+            size_t word4_idx = 0;
+            for (auto word4 : nodes4Ref.mask) {
+                const auto& base_bit_3_idx = ((uint32_t)word4_idx++) * 64;
+                for (; word4 != 0; word4 &= word4 - 1) {
+                    const auto& bit_3_index = base_bit_3_idx + (uint32_t)count_trailing_zeros(word4);
+                    const auto& nodes3Ref   = nodes4Ref.nodes.at(bit_3_index);
+                    write_data_arr<uint64_t>(fp, nodes3Ref.mask, 8);
+                    // Write uncompressed node values, 6 means no compression
+                    write_data<uint8_t>(fp, 6);
+                    write_data_arr<float>(fp, nodes3Ref.data, 512);
+                }
+            }
+        }
+    }
+}
+
+void VdbWriter::m_WriteMetadata(FILE* fp) {
+    // Number of entries
+    write_data<uint32_t>(fp, 3);
+    write_meta_string(fp, "class", "unknown");
+    write_meta_string(fp, "file_compression", "none");
+    write_meta_string(fp, "name", "density");
+}
+
+void VdbWriter::m_WriteTransform(FILE* fp, Mat4x4 mat) {
+    write_name(fp, "AffineMap");
+    //write_data_arr<double>(fp, &mat[0][0], 16);
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            write_data<double>(fp, mat[i][j]);
+        }
+    }
+}
+
+void VdbWriter::m_WriteGrid(FILE* fp, VDB* vdb, Mat4x4 mat) {
+    // Grid name
+    write_name(fp, "density");
+    // Grid type
+    write_name(fp, "Tree_float_5_4_3");
+    // No instance parent
+    write_data<uint32_t>(fp, 0);
+    // Grid descriptor stream position
+    uint64_t stream_pos = m_GetFilePos();
+    write_data<uint64_t>(fp, stream_pos + sizeof(uint64_t) * 3);
+    write_data<uint64_t>(fp, 0);
+    write_data<uint64_t>(fp, 0);
+    // No compression
+    write_data<uint32_t>(fp, 0);
+    m_WriteMetadata(fp);
+    m_WriteTransform(fp, mat);
+    m_WriteTree(fp, vdb);
+}
+
+void VdbWriter::m_WriteVdb(FILE* fp, VDB* vdb, Mat4x4 mat) {
+    // Magic number
+    std::array<uint8_t, 8> header = {0x20, 0x42, 0x44, 0x56, 0x0, 0x0, 0x0, 0x0};
+    write_ptr(fp, header.data(), sizeof(uint8_t), header.size());
+    // File version
+    write_data<uint32_t>(fp, 224);
+    // Library version (we're just gonna pretend we're OpenVDB 8.1)
+    write_data<uint32_t>(fp, 8);  // major
+    write_data<uint32_t>(fp, 1);  // minor
+    // We do not have grid offsets
+    write_data<uint8_t>(fp, 0);
+    // Temporary UUID
+    write_string(fp, "00000000-0000-0000-0000-000000000000");
+    // No metadata for now
+    write_data<uint32_t>(fp, 0);
+    // One grid
+    write_data<uint32_t>(fp, 1);
+    m_WriteGrid(fp, vdb, mat);
+}
+
+bool VdbWriter::m_OpenFileForWriting(const std::string& vFilePathName) {
+#if _MSC_VER
+    lastError = fopen_s(&m_File, vFilePathName.c_str(), "wb");
+#else
+    m_File    = fopen(vFilePathName.c_str(), "wb");
+    lastError = m_File ? 0 : errno;
+#endif
+    if (lastError != 0)
+        return false;
+    return true;
+}
+
+void VdbWriter::m_CloseFile() { fclose(m_File); }
+
+long VdbWriter::m_GetFilePos() const { return ftell(m_File); }
+
+void VdbWriter::m_SetFilePos(const long& vPos) {
+    //  SEEK_SET	Beginning of file
+    //  SEEK_CUR	Current position of the file pointer
+    //	SEEK_END	End of file
+    fseek(m_File, vPos, SEEK_SET);
+}
+
+}  // namespace vdb
---
+++ main.cpp
@@ -0,0 +1,147 @@
+#include "VdbWriter.h"
+#include <cstdint>   // int32_t
+#include <chrono>    // std::chrono
+#include <iostream>  // std::cout
+
+//#define USE_ANIMATED_WAVE
+#define JULIA_REVOLUTE
+//#define USE_VDB_WRITER
+
+#ifdef JULIA_REVOLUTE
+static double mix(const double& x, const double& y, const double& a) { return x * (1.0 - a) + y * a; }
+int           main() {
+    const int32_t SIZE       = 375;
+    const double  ZOOM_XZ    = 7.5;
+    const double  ZOOM_Y     = 7.5;
+    const int32_t ITERATIONS = 5;
+    const int32_t FRAMES     = 50;
+
+    double time = 0.0f;
+    double kk, hh, px, pz, an, cx, cy, path, rev_x, rev_y, tmp_x, tmp_y, rev_x_squared, rev_y_squared, df;
+    double rot2D[4] = {1, 0, 0, 1};  // c,-s,s,c for t=0
+
+    std::array<int32_t, 3> offset = {};
+    bool                   first_offset = true;
+    vdb::VdbWriter vdb;
+    vdb.setKeyFrameTimeLoggingFunctor([=](const vdb::KeyFrame& vKeyFrame, const double& vValue) {  //
+        std::cout << "Elapsed time for Frame " << vKeyFrame << "/" << FRAMES << " : " << vValue << " secs " << std::endl;
+    });
+    vdb.startTimeLogging();
+    int32_t cube_color;
+    double  time_step = 6.28318 / (double)FRAMES;
+    for (int32_t f = 0; f < FRAMES; ++f) {
+        vdb.setKeyFrame(f);
+        time += time_step;
+        for (int32_t i = -SIZE; i < SIZE; ++i) {
+            px = ((double)i * 2.0 / (double)SIZE - 1.0) * ZOOM_XZ;
+            for (int32_t k = -SIZE; k < SIZE; ++k) {
+                pz       = ((double)k * 2.0 / (double)SIZE - 1.0) * ZOOM_XZ;
+                an       = std::atan2(px, pz);
+                cx       = mix(0.2, -0.5, std::sin(an * 2.0));
+                cy       = mix(0.5, 0.0, std::sin(an * 3.0));
+                path     = sqrt(px * px + pz * pz) - 3.0;
+                rot2D[0] = std::cos(an + time);
+                rot2D[1] = -std::sin(an + time);
+                rot2D[2] = std::sin(an + time);
+                rot2D[3] = std::cos(an + time);
+                for (int32_t j = -SIZE; j < SIZE; ++j) {
+                    tmp_y = ((double)j * 2.0 / (double)SIZE - 1.0) * ZOOM_Y;
+                    tmp_x = path;
+                    rev_x = rot2D[0] * tmp_x + rot2D[1] * tmp_y;  // rx2d
+                    rev_y = rot2D[2] * tmp_x + rot2D[3] * tmp_y;  // ry2d
+                    kk    = 1.0;
+                    hh    = 1.0;
+                    for (int32_t idx = 0; idx < ITERATIONS; ++idx) {
+                        rev_x_squared = rev_x * rev_x;
+                        rev_y_squared = rev_y * rev_y;
+                        hh *= 4.0 * kk;
+                        kk = rev_x_squared + rev_y_squared;
+                        if (kk > 4.0) {
+                            break;
+                        }
+                        rev_y = 2.0 * rev_x * rev_y + cy;
+                        rev_x = rev_x_squared - rev_y_squared + cx;
+                    }
+                    df = sqrt(kk / hh) * std::log10(kk);
+                    if (std::abs(df) - 0.01 < 0.0) {
+                        if (first_offset) {
+                            first_offset = false;
+                            offset[0]    = i;
+                            offset[1]    = k;
+                            offset[2]    = j;
+                        }
+                        cube_color = (int32_t)((std::sin(rev_x + rev_y) * 0.5 + 0.5) * 6.0) + 249;
+                        vdb.addVoxelDensity(i + SIZE - offset[0], k + SIZE - offset[1], j + SIZE - offset[2], 1.0f);
+                    }
+                }
+            }
+        }
+    }
+    vdb.stopTimeLogging();
+    vdb.saveToFile("julia_revolute.vdb");
+    vdb.printStats();
+}
+#endif
+
+#ifdef USE_ANIMATED_WAVE
+int main() {
+    const int32_t SIZE      = 150;
+    const int32_t OFFSET    = SIZE;
+    const float   Z_SCALE   = 0.5f;
+    const int32_t FRAMES    = 5;
+    const float   len_ratio = 1.0f / (SIZE * SIZE);
+    // vox::VoxWriter vox;
+    vdb::VdbWriter vdb;
+    vdb.setKeyFrameTimeLoggingFunctor([](const vdb::KeyFrame& vKeyFrame, const double& vValue) {  //
+        std::cout << "Elapsed time for Frame " << vKeyFrame << " : " << vValue << " secs" << std::endl;
+    });
+    vdb.startTimeLogging();
+    float time = 0.0f;
+    for (int32_t f = 0; f < FRAMES; ++f) {
+        vdb.setKeyFrame(f);
+        for (int32_t i = -SIZE; i < SIZE; ++i) {
+            for (int32_t j = -SIZE; j < SIZE; ++j) {
+                float   len        = (i * i + j * j) * len_ratio;
+                int32_t pz         = (int32_t)((std::sin(len * 10.0 + time) * 0.5 + 0.5) * (std::abs(50.0f - 25.0f * len)) * Z_SCALE);
+                int32_t cube_color = (int32_t)(len * 100.0) % 255 + 1;
+                vdb.addVoxelDensity(i + SIZE, j + SIZE, pz, 1.0f);
+            }
+        }
+        time += 0.5f;
+    }
+    vdb.stopTimeLogging();
+    vdb.saveToFile("wave.vdb");
+    vdb.printStats();
+}
+#endif
+
+#ifdef USE_VDB_WRITER
+int main() {
+    vdb::VdbWriter vdb;
+    const uint32_t R = 128;
+    const uint32_t D = R * 2;
+    for (uint32_t z = 0; z < D; ++z) {
+        for (uint32_t y = 0; y < D; ++y) {
+            for (uint32_t x = 0; x < D; ++x) {
+                const auto& px             = x - R;
+                const auto& py             = y - R;
+                const auto& pz             = z - R;
+                const auto& length_squared = px * px + py * py + pz * pz;
+                if (length_squared < R * R) {
+                    vdb.addVoxelDensity(x, y, z, 1.0f);
+                    float fx = (float)px;
+                    float fy = (float)py;
+                    float fz = (float)pz;
+                    const auto& len = sqrtf(fx * fx + fy * fy + fz * fz);
+                    fx /= len;
+                    fy /= len;
+                    fz /= len;
+                    vdb.addVoxelNormal(x, y, z, {fx, fy, fz});
+                    vdb.addVoxelColor(x, y, z, {0.8, 0.5, 0.2, 1.0});
+                }
+            }
+        }
+    }
+    vdb.saveToFile("sphere.vdb");
+}
+#endif
